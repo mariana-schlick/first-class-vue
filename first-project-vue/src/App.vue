36163c7c06e9624f046be971ed1db1ded5cd449b
mariana-schlick <mariana.schlick@gmail.com>
2024-12-29 20:44:35 -0300

    Adição de comunicação de componentes

--- first-project-vue/src/App.vue
+++ first-project-vue/src/App.vue
@@ -4,6 +4,8 @@ import ComDados from "./components/ComDados.vue"
 import ComDadosReativosPrimitivos from "./components/ComDadosReativosPrimitivos.vue";
 import ComDadosReativosNaoPrimitivos from "./components/ComDadosReativosNaoPrimitivos.vue";
 import DataBinding from "./components/DataBinding.vue";
+import ComProps from "./components/ComProps.vue";
+import EscutaEvento from "./components/EscutaEvento.vue";
 </script>
 
 <template>
@@ -13,8 +15,6 @@ import DataBinding from "./components/DataBinding.vue";
   <ComDadosReativosPrimitivos/>
   <ComDadosReativosNaoPrimitivos/>
   <DataBinding/>
+  <ComProps item="Borracha" :preco="2.50"/> <!-- Comunicação entre componente PAI com componente FILHO -->
+  <EscutaEvento/>
 </template>
-
-<style scoped>
-
-</style>
---
+++ first-project-vue/src/components/EmiteEvento.vue
@@ -0,0 +1,25 @@
+<!-- Comunicação entre FILHO e PAI -->
+<!-- Componente filho que emite evento -->
+<template>
+    <div class="componente">
+        <h1>Componente que Emite Evento</h1>
+        <p>{{ item }} (R${{ preco.toFixed(2) }})</p>
+        <button @click="adicionar">Adicionar ao carrinho</button>
+    </div>
+</template>
+
+<script setup>
+import {defineProps, defineEmits} from "vue"
+
+const props = defineProps({
+    item:String,
+    preco:Number,
+})
+
+const emit = defineEmits(["foiAdicionado"])
+
+function adicionar(){
+    emit("foiAdicionado", props.preco)
+}
+
+</script>
