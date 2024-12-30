473a9f8c88e6f4f83e2c22f5001d12077a8b222b
mariana-schlick <mariana.schlick@gmail.com>
2024-12-30 09:11:08 -0300

    Propriedades Computadas, Assistidas e Renderização Condicional

--- first-project-vue/src/App.vue
+++ first-project-vue/src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
-import PrimeiroComponente from "./components/PirmeiroComponente.vue"
+import PrimeiroComponente from "./components/PrimeiroComponente.vue"
 import ComDados from "./components/ComDados.vue"
 import ComDadosReativosPrimitivos from "./components/ComDadosReativosPrimitivos.vue";
 import ComDadosReativosNaoPrimitivos from "./components/ComDadosReativosNaoPrimitivos.vue";
 import DataBinding from "./components/DataBinding.vue";
 import ComProps from "./components/ComProps.vue";
 import EscutaEvento from "./components/EscutaEvento.vue";
+import PropriedadesComputadas from "./components/PropriedadesComputadas.vue"
+import PropriedadesAssistidas from "./components/PropriedadesAssistidas.vue";
+import RenderizacaoCondicional from "./components/RenderizacaoCondicional.vue";
 </script>
 
 <template>
@@ -17,4 +20,7 @@ import EscutaEvento from "./components/EscutaEvento.vue";
   <DataBinding/>
   <ComProps item="Borracha" :preco="2.50"/> <!-- Comunicação entre componente PAI com componente FILHO -->
   <EscutaEvento/>
+  <PropriedadesComputadas/>
+  <PropriedadesAssistidas/>
+  <RenderizacaoCondicional nome="Mariana" :qtde-cachorros="2"/>
 </template>
---
+++ first-project-vue/src/components/PropriedadesAssistidas.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="componente">
+        <h1>Componente com Propriedades Assistidas</h1>
+        <p>{{ idade }}</p>
+        <label>Ano de Nascimento: </label>
+        <input type="number" v-model="anoNascimento" min="1900"/>
+        <label>Ano Atual: </label>
+        <input type="number" v-model="anoAtual" min="2000"/>
+    </div>
+</template>
+
+<script setup>
+import {ref, watch} from 'vue'
+
+const anoNascimento = ref(1900)
+const anoAtual = ref(2000)
+const idade = ref(0)
+
+watch(anoAtual, (ano)=>{
+    idade.value = ano - anoNascimento.value
+})
+
+watch(anoNascimento, (ano)=>{
+    idade.value = anoAtual.value - ano
+})
+
+</script>
+
+<style>
+label{
+    margin: 10px;
+}
+</style>
+
+<!-- Usando a função Watch Effect -->
+<!--
+<script setup>
+import { watchEffect } from 'vue'
+
+const anoNascimento = ref(1900)
+const anoAtual = ref(2000)
+const idade = ref(0)
+
+watchEffect(()=>{
+    idade.value = anoAtual.value - anoNascimento.value
+})
+</script>
+-->
