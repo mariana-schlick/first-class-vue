785dc07c889cd1591adb56e7de545446e4053c84
mariana-schlick <mariana.schlick@gmail.com>
2024-12-30 10:01:06 -0300

    Slots

--- first-project-vue/src/App.vue
+++ first-project-vue/src/App.vue
@@ -10,6 +10,7 @@ import PropriedadesComputadas from "./components/PropriedadesComputadas.vue"
 import PropriedadesAssistidas from "./components/PropriedadesAssistidas.vue";
 import RenderizacaoCondicional from "./components/RenderizacaoCondicional.vue";
 import RenderizacaoListas from "./components/RenderizacaoListas.vue";
+import Slots from "./components/Slots.vue";
 
 const borracha = "borracha"
 const precoBorracha = 2.50
@@ -35,4 +36,14 @@ const cachorrosDoJoao = [
   <PropriedadesAssistidas/>
   <RenderizacaoCondicional nome="Mariana" :qtde-cachorros="2"/>
   <RenderizacaoListas nome="João" :cachorros="cachorrosDoJoao"/>
+  <Slots>
+    <template v-slot:icone>
+      <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-alert-square-rounded">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M12 3c7.2 0 9 1.8 9 9s-1.8 9 -9 9s-9 -1.8 -9 -9s1.8 -9 9 -9z" />
+        <path d="M12 8v4" />
+        <path d="M12 16h.01" />
+      </svg>
+    </template>
+  </Slots>
 </template>
---
+++ first-project-vue/src/components/Slots.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="componente">
+        <h1>Componente com Slots</h1>
+        <button>
+            <slot name="icone"/>
+            <slot name="texto">
+                Botão <!-- Valor Padrão -->
+            </slot>
+        </button>
+    </div>
+</template>
